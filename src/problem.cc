7f88fbe7cdce991b4ff545c3af6e5fcb1d448aab
Evan Klitzke <evan@eklitzke.org>
2018-01-04 05:31:02 -0800

    reflow some text

--- src/problem.cc
+++ src/problem.cc
@@ -93,8 +93,7 @@ ProblemManager::ProblemManager() {
   AddSolution(1, 5, []() {
     Buffer buf(
         "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear "
-        "a "
-        "cymbal");
+        "a cymbal");
     buf.xor_string("ICE");
     return buf.encode_hex() ==
            "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a262263242"
